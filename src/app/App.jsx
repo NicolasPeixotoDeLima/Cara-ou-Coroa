f8351a25276cf6ef94640829fe466549b5ef81ac
Your Name <you@example.com>
2025-08-24 18:10:39 -0300

    finished

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,49 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState("");
+  const [animating, setAnimating] = useState(false);
+
+  const jogarMoeda = () => {
+    setAnimating(true);
+
+    // espera o tempo da animação e depois sorteia
+    setTimeout(() => {
+      const lados = ["cara", "coroa"];
+      const sorteio = lados[Math.floor(Math.random() * lados.length)];
+      setResult(sorteio);
+      setAnimating(false);
+    }, 2000); // 2 segundos de "giro"
+  };
+
   return (
     <>
-      <div className=""></div>
+      <div className="TitleComponent">
+        <h1 className="title">Flip the coin</h1>
+        <h3 className="desc">Press the coin or the button to flip the coin</h3>
+      </div>
+
+      <div className="coin">
+        <div className={`HorT ${animating ? "flip" : ""}`}>
+          <img
+            src={result === "coroa" ? "/tails.svg" : "/heads.svg"}
+            alt="moeda"
+          />
+        </div>
+
+        <div className="shadow">
+          <img src="/shadow.svg" alt="sombra" />
+        </div>
+
+        <h2 className="result">{result && `${result}`}</h2>
+
+        <div>
+          <button onClick={jogarMoeda} disabled={animating} className="botao">
+            Jogar
+          </button>
+        </div>
+      </div>
     </>
   );
 }
